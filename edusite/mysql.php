bcec3b91a12f48b140d9924241a5878ae2c0de97
pranjalkes19 <71693020+pranjalkes19@users.noreply.github.com>
2021-11-23 18:44:21 +0530

    Add files via upload

---
+++ edusite/mysql.php
@@ -0,0 +1,12 @@
+<?php
+
+// Connecting with MySQL
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';//with XAMPP password is '' else change your password accordingly
+$database = 'edusite';
+
+$con = mysqli_connect($servername,$username,$password,$database);
+
+?>
